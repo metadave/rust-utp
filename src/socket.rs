metadave/rust-utp

commit fd77155261c57e4c12aa991ed920d873b162ccaa

Fixes UtpSocket.connect if 'State' packet is dropped

src/socket.rs
@@ -201,6 +201,10 @@ pub struct UtpSocket {
     last_congestion_update: SteadyTime,
 
     retries: u32,
+
+    /// The first 'State' packet we sent if we are a server (it may
+    /// need to be resent if the network dropped it).
+    state_packet: Option<Packet>,
 }
 
 impl UtpSocket {
@@ -253,6 +257,7 @@ impl UtpSocket {
             user_read_timeout: 0,
             last_congestion_update: SteadyTime::now(),
             retries: 0,
+            state_packet: None,
         }
     }
 
@@ -305,11 +310,11 @@ impl UtpSocket {
         packet.set_connection_id(socket.receiver_connection_id);
         packet.set_seq_nr(socket.seq_nr);
 
-        let mut len = 0;
         let mut buf = [0; BUF_SIZE];
-
         let mut syn_timeout = socket.congestion_timeout;
-        for _ in 0..MAX_SYN_RETRIES {
+        let mut syn_retries = 0;
+
+        while syn_retries < MAX_SYN_RETRIES {
             packet.set_timestamp_microseconds(now_microseconds());
 
             // Send packet
@@ -323,29 +328,34 @@ impl UtpSocket {
                   .set_read_timeout(Some(Duration::from_millis(syn_timeout)))
                   .expect("Error setting read timeout");
             match socket.socket.recv_from(&mut buf) {
-                Ok((read, src)) => {
-                    socket.connected_to = src;
-                    len = read;
-                    break;
-                }
+                Ok((read, addr)) => {
+                    let packet = try!(Packet::from_bytes(&buf[..read]).or(Err(SocketError::InvalidPacket)));
+
+                    socket.connected_to = addr;
+
+                    if packet.get_type() != PacketType::State {
+                        // The network might have dropped the `State` packet
+                        // from the peer, so we need to ask for it again.
+                        syn_retries += 1;
+                        continue;
+                    }
+
+                    try!(socket.handle_packet(&packet, addr));
+
+                    return Ok(socket);
+                },
                 Err(ref e) if (e.kind() == ErrorKind::WouldBlock ||
                                e.kind() == ErrorKind::TimedOut) => {
                     debug!("Timed out, retrying");
                     syn_timeout *= 2;
+                    syn_retries += 1;
                     continue;
                 }
                 Err(e) => return Err(e),
             };
         }
 
-        let addr = socket.connected_to;
-        let packet = try!(Packet::from_bytes(&buf[..len]).or(Err(SocketError::InvalidPacket)));
-        debug!("received {:?}", packet);
-        try!(socket.handle_packet(&packet, addr));
-
-        debug!("connected to: {}", socket.connected_to);
-
-        Ok(socket)
+        Err(Error::from(SocketError::ConnectionTimedOut))
     }
 
     /// If you have already prepared UDP sockets at each end (e.g. you're doing
@@ -1064,15 +1074,18 @@ impl UtpSocket {
                 self.state = SocketState::Connected;
                 self.last_dropped = self.ack_nr;
 
-                Ok(Some(self.prepare_reply(packet, PacketType::State)))
+                self.state_packet = Some(self.prepare_reply(packet, PacketType::State));
+                Ok(self.state_packet.clone())
             }
             (SocketState::Connected, PacketType::Syn) if self.connected_to == src => {
                 // The other end might have sent another Syn packet because
                 // a reply to the first one did not arrive within a timeout
                 // caused by network congestion.
-                Ok(None)
+                Ok(self.state_packet.clone())
             }
-            (_, PacketType::Syn) => Ok(Some(self.prepare_reply(packet, PacketType::Reset))),
+            (_, PacketType::Syn) => {
+                Ok(Some(self.prepare_reply(packet, PacketType::Reset)))
+            }
             (SocketState::SynSent, PacketType::State) => {
                 self.connected_to = src;
                 self.ack_nr = packet.seq_nr();
@@ -3054,6 +3067,7 @@ mod test {
         let mut server = iotry!(listener.accept()).0;
 
         assert_eq!(iotry!(server.send_to(&TX_BUF)), TX_BUF.len());
+        let _ = server.flush();
 
         assert!(client_t.join().is_ok());
     }

commit 99648c028db3046c8307e03dea4c70b390cf1bea

fix: `socket::test::test_receive_unexpected_reply_type_on_connect` test
Commit fd77155261c57e4c12aa991ed920d873b162ccaa introduced code that
made a test that was previously passing to fail. This usually means
regression.

I could very very easily add code to make the test pass, but the current
behaviour looks more right to me than the test described behaviour.
Therefore, I'm changing the test.

I won't revert the commit that introduced the "regression" because it
fixes an important bug that wasn't tested. Peter found a lot of issues
when he started emulating dropped packets and writing
more-than-just-unit-tests (something in the direction of integration
tests). Something that was seemingly being neglected previously.

src/socket.rs
@@ -2510,8 +2510,8 @@ mod test {
         });
 
         match UtpSocket::connect(server_addr) {
-            Err(ref e) if e.kind() == ErrorKind::ConnectionRefused => (), // OK
-            Err(e) => panic!("Expected ErrorKind::ConnectionRefused, got {:?}", e),
+            Err(ref e) if e.kind() == ErrorKind::TimedOut => (), // OK
+            Err(e) => panic!("Expected ErrorKind::TimedOut, got {:?}", e),
             Ok(_) => panic!("Expected Err, got Ok"),
         }
 